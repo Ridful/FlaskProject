478488dd3cc3e8d8d7a97447d5b131bb3ffaec33
Ridful <riengelful@gmail.com>
2023-10-27 16:34:44 +0200

    Added more listener routes & class structures

--- application/endpoints/listener.py
+++ application/endpoints/listener.py
@@ -1,26 +1,58 @@
 from flask import Blueprint, request
+from ..utils.agent import Agent, Results
+
 
 listener_bp = Blueprint('listener', __name__, static_folder="static", template_folder="templates", url_prefix="/")
+
 
 @listener_bp.route("/reg", methods=["POST"])
 def registerAgent():
     #username = request.json["name"]
     if request.method == "POST" and request.is_json:
-        
         data = request.get_json()
         
-        #Register data about new agent to database
         name = data["name"]
-        ip = request.remote_addr
+        remote_agent_ip = request.remote_addr
         
+        new_agent = Agent(name, remote_agent_ip)
+        print(f"New Agent: Name:{new_agent.name} | IP:{new_agent.ip} | Joined:{new_agent.joindate} | UUID:{new_agent.uuid}")
+        
+        #ip = request.remote_addr
         #encrypted key to use for comms with endpoints for AAA reasons
-        formatted_msg = "api-key"
-        
+        formatted_msg = new_agent.uuid #"api-key"
         return (formatted_msg, 200)
-
+    
     else:
         return ("register failed", 204)
     
-#@listener_bp.route("/tasks/<>", methods=["GET"])
-#def serveTasks(name):
-#    return
+    
+@listener_bp.route("/results", methods=["POST"])
+def agentActionResults():
+    if request.method == "POST" and request.is_json:
+        
+        data = request.is_json()
+        
+        agentPostResults = Results
+        #agentPostResults.
+        
+        print(data)
+        
+        #uuid  ip  past_task  task_result
+        #print(f"Results:{new_agent.name} | IP:{new_agent.ip} | Joined:{new_agent.joindate} | UUID:{new_agent.uuid}")
+        print(f"{agentPostResults}")
+        
+        return "oh pass pass"
+    else:
+        return ("failed to return the results", 404)
+    
+
+@listener_bp.route("/tasks/<uuid>", methods=["GET"])
+def serveTasks(uuid):
+    
+    return f"Hello UUID:{uuid}!"
+
+
+@listener_bp.route("/download/<filename>", methods=["GET"])
+def serveDownload(filename):
+    
+    return f"This is the location of the file: {filename}!"
--- application/utils/agent.py
+++ application/utils/agent.py
@@ -1,7 +1,26 @@
-
+from datetime import datetime
+import hashlib
 
 class Agent:
-    def __init__(self, name, ip_address, joindate):
+    def __init__(self, name, ip):
         self.name = name
-        self.ip = ip_address
-        self.joindate = joindate
+        self.ip = ip
+        self.joindate = datetime.now()  # .strptime("%d/%m/%Y, %H:%M:%S")
+        self.uuid = self.generate_unique_id()
+        
+    def generate_unique_id(self):
+        posix_timestamp = str(int(self.joindate.timestamp()))
+        unique_id = (posix_timestamp + "sv_SE" + str("-idk")).encode('ascii')
+        hash_object = hashlib.md5(unique_id)
+        return hash_object.hexdigest()
+    
+    def display_result(self):
+        print(f"Name: {self.name}, IP: {self.ip}, joindate: {self.joindate}, uuid: {self.uuid}")
+        
+class Results(Agent):
+    def __init__(self, name, ip, result):
+        super().__init___(name, ip)    #this will init the parent
+        self.result = result
+    
+    def display_result(self):
+        print(f"Name: {self.name}, IP: {self.ip}, Result: {self.result}")
